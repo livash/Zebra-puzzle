600e530c2abf2841fc96a938668b11f521e0b717
olena ivashyna <olena.ivashyna@gmail.com>
2013-08-30 13:58:22 -0700

    COnstants in the game class

--- game.rb
+++ game.rb
@@ -1,16 +1,35 @@
 
 class Game
+  COLORS = %w{red blue white yellow}
+  NATIONALITY = %w{Brit Sweede Dane Norwegian German}
+  PETS = %w{dos birds cats horse fish}
+  CIGARETTE_BRAND = ["Pall Malls", "Dunhill", "Blends", "Bluemasters", "Prince"]
+  DRINKS = %w{coffee milk beer water}
   attr_accessor :question, :clues 
   
   def initialize(str="")
-    @question, @clues = Game.parse_data(str)
+    data = Game.parse_data(str)
+    @question = data.first
+    @clues = data.last
   end
   
   def self.parse_data(str)
+    res_question = ""
+    res_clues = []
+    lines = str.split("\n")
+    lines.each_with_index do |line, idx|
+      if idx == 0
+        res_question = lines.split(" ")[-2] 
+      else
+        # parse_clue returns {property: value}
+        res_clues << Game.parse_clue(line)
+      end
+    end
     
+    [res_question, res_clues]
   end
   
-  def method_name
+  def self.parse_clue(str)
     
   end
 end
--- zebra_puzzle.rb
+++ zebra_puzzle.rb
@@ -3,7 +3,7 @@
 require_relative 'game'
 
 def solve_zebra_puzzle(file_name)
-  contnet = File.read(file_name)
+  content = File.read(file_name)
   game = Game.new(content)
   
   
